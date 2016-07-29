0a5a939ebd83686d943e32302ba47bcfa7f625dc
Gabriel Pedro <gpedro831@gmail.com>
2016-07-28 21:40:49 -0400

    respondendo mensagem

--- jokes.js
+++ jokes.js
@@ -2,26 +2,20 @@ const express = require('express');
 const got = require('got');
 const router = express.Router();
 const joke = require('./joke');
-const send = (data) => {
-  got.post(data.response_url, {
-    body: {
-      "text": "Portuguese Jokes",
-      "attachments": [
-        {
-            "text": joke.get()
-        }
-      ]
-    }
-  });
-}
 
 router.get('/', function (req, res, next) {
   return res.redirect('https://github.com/gpedro/slack-portuguese-jokes/');
 });
 
 router.post('/', function(req, res, next) {
-  send(res.body);
-  return res.status(200);
+  return res.status(200).json({
+    "response_type": "in_channel",
+    "attachments": [
+      {
+          "text": joke.get()
+      }
+    ]
+  });
 });
 
 module.exports = router;
